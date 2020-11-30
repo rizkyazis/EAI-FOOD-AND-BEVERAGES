fafc22c6d7c529a8689eeb4b6d31c315119aa3f6
fakhririfqifirdaus <fakhririfi@gmail.com>
2020-11-30 12:49:12 +0700

    Template boi

--- app/Http/Controllers/API/MenuController.php
+++ app/Http/Controllers/API/MenuController.php
@@ -11,27 +11,28 @@ use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    public function index(){
-        try{
+    public function index()
+    {
+        try {
             $menu = Menu::all();
             $host = request()->getHttpHost();
             $result = [];
 
-            foreach ($menu as $item){
-                array_push($result,[
-                    'id'=>$item->id,
-                    'name'=>$item->name,
-                    'image'=>$host.'/images/menu/'.$item->image,
-                    'type'=>$item->type,
-                    'price'=>$item->price
-                    ]);
+            foreach ($menu as $item) {
+                array_push($result, [
+                    'id' => $item->id,
+                    'name' => $item->name,
+                    'image' => $host . '/images/menu/' . $item->image,
+                    'type' => $item->type,
+                    'price' => $item->price
+                ]);
             }
             return response()->json([
                 'status' => true,
                 'message' => 'Data Found',
                 'menu' => $result
             ]);
-        }catch (QueryException $e){
+        } catch (QueryException $e) {
             return response()->json([
                 'status' => false,
                 'message' => $e->getMessage(),
@@ -40,11 +41,12 @@ class MenuController extends Controller
         }
     }
 
-    public function detail($id){
-        try{
+    public function detail($id)
+    {
+        try {
             $menu = Menu::find($id);
             $host = request()->getHttpHost();
-            $menu->image = $host.'/images/menu/'.$menu->image;
+            $menu->image = $host . '/images/menu/' . $menu->image;
             $menu->ingredients = $menu->ingredients;
             $menu->category_id = Category::find($menu->category_id);
             return response()->json([
@@ -52,7 +54,7 @@ class MenuController extends Controller
                 'message' => 'Data Found',
                 'menu' => [$menu]
             ]);
-        }catch (QueryException $e){
+        } catch (QueryException $e) {
             return response()->json([
                 'status' => false,
                 'message' => $e->getMessage(),
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,26 +2,73 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1 hrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="icon" type="image/x-icon" href="{{'/style/assets/img/favicon.ico'}}" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/style/css/styles.css') }}" rel="stylesheet"/>
 
     @stack('css')
 </head>
-<body>
+<body id="page-top">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="{{'/style/assets/img/navbar-logo.svg'}}" alt=""/></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars ml-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav text-uppercase ml-auto">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#order">Order</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 <main class="py-4">
     @yield('content')
 </main>
+<!-- Footer-->
+<footer class="footer py-4">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 text-lg-left">Copyright © Your Website 2020</div>
+            <div class="col-lg-4 my-3 my-lg-0">
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-lg-4 text-lg-right">
+                <a class="mr-3" href="#!">Privacy Policy</a>
+                <a href="#!">Terms of Use</a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<!-- Core theme JS-->
+<script src="{{'/style/js/scripts.js'}}" defer></script>
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
 @stack('script')
